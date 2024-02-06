b324a055a7298e05319aecc749d51585c32af7c6
Angel Chang <angel.h.chang@gmail.com>
2024-02-06 14:28:49 +0000

    wip

--- rplot/I2.R
+++ rplot/I2.R
@@ -4,7 +4,7 @@ df<- filtered_extracted_sex
 
 library(tidyverse)
 
-# Assuming 'df' is your original dataframe.
+
 
 # Modify the dataframe in place to add the number of males and the proportion of females
 df <- df %>%
@@ -17,17 +17,17 @@ df <- df %>%
   ungroup() %>%
   # Select distinct rows based on 'nct_id' to remove duplicates
   distinct(nct_id, .keep_all = TRUE) %>%
-  # Optionally, you can select the columns you want to keep
+ 
   select(nct_id, Num_Females, Num_Males, Proportion_Females)
 
-# The dataframe 'df' now contains one row per 'nct_id' with the number of males and the proportion of females.
+
 print(df)
 install.packages("meta")
 library(meta)
 df <- df %>%
   mutate(Total_Participants = Num_Females + Num_Males)
 
-# Now 'df' will have a new column 'Total_Participants' with the total number of participants for each trial.
+
 print(df)
 
 meta_analysis <- metaprop(df$Num_Females,df$Total_Participants, sm = "PAS", method.tau.ci="J", data=df)
@@ -35,7 +35,7 @@ meta_analysis <- metaprop(df$Num_Females,df$Total_Participants, sm = "PAS", meth
 # Print the meta-analysis results
 print(meta_analysis)
 
-# Print the random effects model results
+
 random_effects <- meta_analysis$random
 cat("Random Effects Pooled Proportion:", random_effects$TE, "\n95% CI =", random_effects$lower, "to", random_effects$upper, "\n\n")
 
